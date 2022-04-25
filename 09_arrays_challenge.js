c12b4c50fec3007c1f929b80213ccbc600c49ac7
Conor Hope <c_hope@hotmail.co.uk>
2022-04-25 15:44:04 +0100

    Completed step 9 'Arrays'

---
+++ 09_arrays_challenge.js
@@ -0,0 +1,9 @@
+const addToBatch = (array, number) => {
+  if (array.length >= 5) {
+    return array;
+  } else {
+    return array.concat(number);
+  }
+}
+
+module.exports = addToBatch;
---
+++ 09_arrays_exercise_1.js
@@ -0,0 +1,8 @@
+const names = ['Conor', 'Alexis', 'Makers'];
+
+console.log(names.length)
+console.log(names[1])
+
+const newNames = names.concat('Leo')
+
+console.log(newNames)
