4ab4810c8311d55f46b94139e0db5340056ed483
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-25 14:29:29 +0100

    Loops chapter complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,10 @@
-const jizzbuzz = (n) for (var i = 1; i < n; i++) {
-  if (i % 15 == 0) console.log("FizzBuzz");
-  else if (i % 3 == 0) console.log("Fizz");
-  else if (i % 5 == 0) console.log("Buzz");
-  else console.log(i);
+const fizzbuzzUntil = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+  }
 }
 
-module.exports = jizzbuzz;
+module.exports = fizzbuzzUntil;
