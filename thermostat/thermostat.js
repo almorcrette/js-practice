1e1e3345f093e3b6b93e1f340e069ad61d9bcb4f
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-27 14:52:17 +0100

    temperature max when power saving is off implemented

--- thermostat/thermostat.js
+++ thermostat/thermostat.js
@@ -5,11 +5,20 @@ class Thermostat {
   };
 
   up(degrees) {
-    if (this.temperature + degrees > 25) {
-      this.temperature = 25;
-      return 'Power saving ON - Maximum temperature = 25';
-    } else {
-      this.temperature += degrees;
+    if (this.powerSavingMode === 'on') {
+      if (this.temperature + degrees > 25) {
+        this.temperature = 25;
+        return 'Power saving ON - Maximum temperature = 25';
+      } else {
+        this.temperature += degrees;
+      };
+    } else if (this.powerSavingMode = 'off') {
+      if (this.temperature + degrees > 32) {
+        this.temperature = 32;
+        return 'Power saving OFF - Maximum temperature = 32';
+      } else {
+        this.temperature += degrees;
+      };    
     };
   };
 
